3b8c299f155ddd4cbbf79fb40bc3203e1c9f4b16
you21979 <you2197901@gmail.com>
2016-10-25 20:23:49 +0900

    add test

--- test/test.js
+++ test/test.js
@@ -1,18 +1,15 @@
+var assert = require("assert");
 var LimitRequestPromise = require('..');
-var lr = new LimitRequestPromise(2,5);
-//var lr = new LimitRequestPromise();
-lr.setup([
-{
-    host:'http://www.example.com',max:1,sec:1
-}
-]);
-var done = function(w){return function(){console.log(w,'done',lr.waitcount)}}
-var list = ['http://www.yahoo.co.jp', 'http://www.google.co.jp'];
-list.forEach(function(v){
-    lr.req(v).then(done(v));
-    lr.req(v).then(done(v));
-    lr.req(v).then(done(v));
-    lr.req(v).then(done(v));
-    lr.req(v).then(done(v));
-})
 
+describe("simple", function() {
+    it("test", function(done) {
+        var lr = new LimitRequestPromise(1, 1);
+        lr.req('http://www.yahoo.co.jp').then(function(res){
+            return lr.req('http://www.yahoo.co.jp').then(function(res){
+                done()
+            })
+        }).catch(function(e){
+            done(e)
+        })
+    });
+});
