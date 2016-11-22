1d9526c53396841145f7821519cacae96b7d9619
you21979 <you2197901@gmail.com>
2016-11-22 23:11:02 +0900

    mod test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,20 @@
 var assert = require("assert");
+var task =require('promise-util-task');
 var LimitRequestPromise = require('..');
 
 describe("simple", function() {
     it("test", function(done) {
+        this.timeout(5000);
         var lr = new LimitRequestPromise(1, 1);
-        lr.req('http://www.yahoo.co.jp').then(function(res){
-            return lr.req('http://www.yahoo.co.jp').then(function(res){
-                done()
-            })
+        var urls = [
+            'http://www.yahoo.co.jp',
+            'http://www.yahoo.co.jp',
+            'http://www.yahoo.co.jp',
+            'http://www.yahoo.co.jp'
+        ]
+        var tasklist = urls.map(function(v){ return function(){ return lr.req(v) } })
+        task.all(tasklist).then(function(){
+            done()
         }).catch(function(e){
             done(e)
         })
